d588b8cb09f6cfb793689552cda1351229aa24b3
Mikita Buinavets <mikita.buinavets@arua.co>
2024-03-16 01:18:26 +0300

    module 4. task 3. fix 2

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -5,7 +5,7 @@ public class DigitsSum {
         String a = String.valueOf(t);
         int result  = 0;
 
-        for (int i = 0; i <= a.length() - 1; i++) {
+        for (int i = 0; i <= a.length(); i++) {
             result += a.charAt(i) - '0';
 
         }
